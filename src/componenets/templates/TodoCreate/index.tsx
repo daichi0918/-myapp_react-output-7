b6991b2924fac7909d7e1aca4b089cba71155a8b
daichi0918 <kayasugadaichi@icloud.com>
2024-10-27 14:18:46 +0900

    todoCreate内の みでの実装一旦完了

--- src/componenets/templates/TodoCreate/index.tsx
+++ src/componenets/templates/TodoCreate/index.tsx
@@ -4,25 +4,74 @@
  * @package templates
  */
 
+import { useContext, useState } from "react";
 import { InputForm } from "../../atoms/InputForm"
 import './index.css';
+import { TodoContext } from "../../../contexts/TodoContext";
+import { useNavigate } from "react-router-dom";
 
 /**
  * @returns {JSX.Element}
  */
 export const TodoCreateTemplate = () => {
+  /**
+   * state定義
+   */
+  const { originalTodoList, setOriginalTodoList, todoListLength, setTodoListLength } = useContext(TodoContext);
+  const [addInputTitle, setAddInputTitle] = useState<string>("");
+  const [addTextAreaContent, setAddTextAreaContent] = useState<string>("");
+
+  const navigate = useNavigate(); 
+
+  /**
+   * action定義
+   */
+  /**
+   * titleの更新処理
+   * @param { React.ChangeEvent<HTMLInputElement>} e
+   */
+  const handleAddInputTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => setAddInputTitle(e.target.value);
+  /**
+   * contentの更新処理
+   * @param { React.ChangeEvent<HTMLInputElement>} e
+   */
+  const handleAddInputContentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => setAddTextAreaContent(e.target.value);
+  /**
+   * todoListの追加発火処理
+   * @param {React.MouseEvent<HTMLButtonElement>} e
+   */
+  const handleCreateTodo = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    if(addInputTitle !== '') {
+      const newId = todoListLength + 1;
+      const newTodoList = [
+        ...originalTodoList, 
+        {
+          id: newId,
+          title: addInputTitle,
+          content: addTextAreaContent
+        }
+      ]
+      setOriginalTodoList(newTodoList);
+      setAddInputTitle('');
+      setAddTextAreaContent('');
+      setTodoListLength(newId);
+
+      navigate("/");
+    }
+  }
   return (
     <>
       <h1 className='title'>Create Todo</h1>
       <form className="contents_container">
         <div className='content'>
-          <input type="text" placeholder="Title" className="title_input"/>
+          <input type="text" placeholder="Title" className="title_input" value={addInputTitle} onChange={handleAddInputTitleChange}/>
         </div>
         <div className='content'>
-          <textarea className="content_input" placeholder="Content"/>
+          <textarea className="content_input" placeholder="Content" value={addTextAreaContent} onChange={handleAddInputContentChange}/>
         </div>
         <div className='content'>
-          <button className="button" type="submit">
+          <button className="button" onClick={handleCreateTodo}>
             Create Todo
           </button>
         </div>
--- src/contexts/TodoContext/index.tsx
+++ src/contexts/TodoContext/index.tsx
@@ -5,17 +5,21 @@
  */
 
 import { useState, createContext, ReactNode } from 'react';
-import { INITIAL_TODO_LIST, TodoType } from '../../const';
+import { INITIAL_TODO_LIST, INITIAL_TODO_LIST_LENGTH, TodoType } from '../../const';
 
 // 型定義
 interface TodoContextType {
   originalTodoList: Array<TodoType>;
   setOriginalTodoList: React.Dispatch<React.SetStateAction<Array<TodoType>>>;
+  todoListLength: number,
+  setTodoListLength: React.Dispatch<React.SetStateAction<number>>;
 }
 
 export const TodoContext = createContext<TodoContextType>({
   originalTodoList: [], // 初期の Todo リスト（空）
   setOriginalTodoList: () => {}, // 初期値として空の関数を設定
+  todoListLength: 2,
+  setTodoListLength: () => {}
 });
 
 /**
@@ -24,9 +28,10 @@ export const TodoContext = createContext<TodoContextType>({
  */
 export const TodoProvider = ({children}: {children: ReactNode}) => {
   const [ originalTodoList, setOriginalTodoList] = useState(INITIAL_TODO_LIST);
+  const [todoListLength, setTodoListLength] = useState<number>(INITIAL_TODO_LIST_LENGTH);
 
   return (
-    <TodoContext.Provider value={{ originalTodoList, setOriginalTodoList}}>
+    <TodoContext.Provider value={{ originalTodoList, setOriginalTodoList, todoListLength, setTodoListLength}}>
       {children}
     </TodoContext.Provider>
   )
