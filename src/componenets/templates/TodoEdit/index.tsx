b5c48f35453026af42337f0462ca6f149ab0ec7b
daichi0918 <kayasugadaichi@icloud.com>
2024-10-31 15:21:56 +0900

    todoEditのhooksとcomponentの切り出し完了

--- src/componenets/templates/TodoEdit/index.tsx
+++ src/componenets/templates/TodoEdit/index.tsx
@@ -10,6 +10,7 @@ import { InputForm } from "../../atoms/InputForm"
 import { TextAreaForm } from "../../atoms/TextAreaForm"
 import { TodoContext } from "../../../contexts/TodoContext";
 import { useNavigate } from "react-router-dom";
+import { useTodoEdit } from "../../../hooks/TodoEdit";
 
 
 /**
@@ -18,60 +19,14 @@ import { useNavigate } from "react-router-dom";
  */
 
 export const TodoEditTemplate = ({ id } : {id: string }) => {
-  /**
-   * state定義
-   */
+  // hooksからstateとactionを取得
   const {
-    originalTodoList,
-    setOriginalTodoList,
-    todoListLength,
-    setTodoListLength,
-
-  } = useContext(TodoContext);
-  // 該当のtodoを取得
-  const targetTodo = originalTodoList.filter((todo) => todo.id === Number(id));
-
-  // 初期値設定
-  const [editInputTitleValue, setEditInputTitleValue] = useState<string>(targetTodo[0].title);
-  const [editTextareaContentValue, setEditTextareaContentValue] = useState<string>(targetTodo[0].content || '');
-
-
-  const navigate = useNavigate();
-
-  /**
-   * action定義
-   */
-  /**
-   * titleの更新処理
-   * @param { React.ChangeEvent<HTMLInputElement>} e
-   */
-    const handleEditInputTitlChange = (e: React.ChangeEvent<HTMLInputElement>) =>
-    setEditInputTitleValue(e.target.value);
-  /**
-   * contentの更新処理
-   * @param { React.ChangeEvent<HTMLInputElement>} e
-   */
-  const handleEditTextareaContentChange = (
-    e: React.ChangeEvent<HTMLTextAreaElement>
-  ) => setEditTextareaContentValue(e.target.value);
-  /**
-   * todoListの更新発火処理
-   * @param {React.MouseEvent<HTMLButtonElement>} e
-   */
-  const handleUpdateTodo = (targetId: string) => {
-    if(editInputTitleValue !== '') {
-      // 疑問：なんで順序変わらないの？？
-      const updatedTodoList = originalTodoList.map((todo) => 
-      todo.id === Number(targetId)
-        ? { ...todo, title: editInputTitleValue, content: editTextareaContentValue }
-        : todo
-      );
-      setOriginalTodoList(updatedTodoList);
-
-      navigate('/');
-    }
-
-  }
+    editInputTitleValue,
+    editTextareaContentValue,
+    handleEditInputTitlChange,
+    handleEditTextareaContentChange,
+    handleUpdateTodo,
+  } = useTodoEdit({id});
   return (
     <>
       <h1 className='title'>TodoEdit</h1>
